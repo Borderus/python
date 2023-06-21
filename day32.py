7ffa983c603f01af549d0cc11512764ad32d4adb
Ben <benback96@gmail.com>
2023-06-21 08:56:21 +0000

    Day 32 Challenge

---
+++ day32.py
@@ -0,0 +1,29 @@
+# Exercise 1
+#timetable = ["Computer Science", "Math", "Art", "Sport"]
+#for i in range(len(timetable)):
+#  print(timetable[i])
+
+#Exercise 2
+#timetable = ["Computer Science", "Math", "Art", "Sport"]
+#for i in range(len(timetable)):
+#  print(timetable[i])
+
+#print();
+
+#timetable[3] = "Watch TV"
+#for lesson in timetable:
+#  print(lesson)
+
+#Fix My Code
+#groceryList = ["bananas", "bread", "milk", "eggs", "juice", "cheese"]
+#print(f"The first grocery item to buy is {groceryList[0]}.")
+
+#Day 32 Challenge
+
+import random
+
+greetingList = ["Hello","Bonjour","Hallo","Hola"]
+
+choice = random.randint(0,len(greetingList)-1)
+
+print(f"{greetingList[choice]}")
